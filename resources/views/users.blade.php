5d2760861936e537f718c911502f9fd37ffc5479
kharlamov39 <kharlamov39@yandex.ru>
2024-06-22 19:00:05 +0300

    up

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,11 +1,29 @@
 <x-layout>
-    @foreach($users as $user)
-        <h2>
-            {{ $user->name }} <br>
-            {{ $user->email }}
-        </h2>
-        @if($user->role_id)
-            <p>Роль {{ $user->role->name }}</p>
-        @endif
-    @endforeach
+    <table class="table mt-5">
+        <h2 class="text-center my-5">Users</h2>
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Role</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+            <tr>
+                <th scope="row">{{ $user->id }}</th>
+                <td> {{ $user->name }}</td>
+                <td> {{ $user->email }}</td>
+                <td>{{ $user->role->name }}</td>
+                <td>
+                    @if(!$user->isAdmin())
+                        <a class="btn btn-danger"> Delete</a>
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </x-layout>
